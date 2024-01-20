5a2dac07e66fd6e07c8906bebe0ee2195411cc87
Changjo Hwang <hchj2005@gmail.com>
2024-01-20 22:24:30 +0900

    fix: 전반적인 페이지 크기 조정

--- src/pages/Loading.tsx
+++ src/pages/Loading.tsx
@@ -4,9 +4,9 @@ import ProgressBar from '@components/Loading/ProgressBar';
 
 const Loading = () => {
   return (
-    <div className="flex flex-col items-center justify-start">
+    <div className="flex flex-col mx-auto max-w-screen-xl px-4 py-2 lg:px-8 lg:py-4">
       <NavBar />
-      <div className="flex h-screen w-screen items-center justify-center">
+      <div className="flex h-screen items-center justify-center">
         <div className="flex flex-col items-center">
           <LoadingCircle />
           <p className="text-lg font-bold">Loading...</p>
