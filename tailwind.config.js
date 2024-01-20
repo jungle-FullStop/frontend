b8f7d0d5f6f754b62c126758150d48272f28f69a
Changjo Hwang <hchj2005@gmail.com>
2024-01-20 14:41:21 +0900

    init: Material Tailwind 세팅 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,41 +7,9 @@ export default withMT({
     {
       pattern: /(bg|text|font|w|h)-+/,
     },
-    {
-      pattern: /btn-(default|delete|normal|large)/,
-    },
   ],
   theme: {
-    colors: {
-      default: '#4B4B4B',
-      orange: '#E7A702',
-      brown: '#C7C1BB',
-      gray: '#D9D9D9',
-      mint: '#C6D8CD',
-      red: '#F96A6A',
-      white: '#FFFFFF',
-      blue: '#6AB4F9',
-      body: '#fffef9',
-      emotion: {
-        0: '#EDEDED',
-        1: '#DE8080',
-        2: '#E9AAAA',
-        3: '#F5E096',
-        4: '#B8D3A2',
-        5: '#99B384',
-      },
-    },
-    extend: {
-      gridTemplateRows: {
-        7: 'repeat(7, minmax(0, 1fr))',
-      },
-      boxShadow: {
-        custom: '0 0 10px 0 rgba(0,0,0,0.25)',
-      },
-      width: {
-        34: '8.5rem',
-      },
-    },
+    extend: {},
   },
   plugins: [],
 });
