b3e1da392335208a2cc6789d4f9f70d104ee2d8c
Changjo Hwang <hchj2005@gmail.com>
2024-01-20 13:39:12 +0900

    init: Material Tailwind 세팅

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,7 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+const withMT = require('@material-tailwind/react/utils/withMT');
+
+export default withMT({
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   safelist: [
     {
@@ -42,4 +44,4 @@ export default {
     },
   },
   plugins: [],
-};
+});
