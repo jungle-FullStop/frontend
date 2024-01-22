917a98fac8b9a2ea71dcf347d60f6790d1dda5c8
Changjo Hwang <hchj2005@gmail.com>
2024-01-22 11:29:11 +0900

    chore: 오탈자 및 불필요한 파일 삭제

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,25 +1,17 @@
-import { useState, useEffect } from "react";
-import Heattmap from "@/components/Home/Grass2";
+import { useState, useEffect } from 'react';
+import Heatmap from '@/components/Home/Grass2';
 import { useNavigate } from 'react-router-dom';
 import { Button } from '@material-tailwind/react';
 
-
 const Home = () => {
   const navigate = useNavigate();
-  
 
-  let [member,setMember] = useState([
-    {name : '황창조',
-    role : 'FE / Leader'},
-    {name : '정준희',
-    role : 'FE'},
-    {name : '박정민',
-    role : 'BE'},
-    {name : '박건률',
-    role : 'BE'},
-  ])
-
-
+  let [member, setMember] = useState([
+    { name: '황창조', role: 'FE / Leader' },
+    { name: '정준희', role: 'FE' },
+    { name: '박정민', role: 'BE' },
+    { name: '박건률', role: 'BE' },
+  ]);
 
   return (
     <div>
@@ -33,18 +25,16 @@ const Home = () => {
           가이드라인 생성
         </Button>
       </div>
-  
+
       <div className="bg-white py-24 sm:py-32">
         <div className="mx-auto grid max-w-7xl gap-x-8 gap-y-20 px-6 lg:px-8 xl:grid-cols-3">
           <div className="max-w-2xl">
             <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
               TIL HISTORY
             </h2>
-            <p className="mt-6 text-lg leading-8 text-gray-600">
-              동해물과 백두산이 마르고 닳도록
-            </p>
+            <p className="mt-6 text-lg leading-8 text-gray-600">동해물과 백두산이 마르고 닳도록</p>
           </div>
-  
+
           <ul
             role="list"
             className="grid gap-x-8 gap-y-12 sm:grid-cols-2 sm:gap-y-16 xl:col-span-2"
@@ -61,9 +51,7 @@ const Home = () => {
                     <h3 className="text-base font-semibold leading-7 tracking-tight text-gray-900">
                       {a.name}
                     </h3>
-                    <p className="text-sm font-semibold leading-6 text-indigo-600">
-                      {a.role}
-                    </p>
+                    <p className="text-sm font-semibold leading-6 text-indigo-600">{a.role}</p>
                   </div>
                 </div>
               </li>
@@ -71,11 +59,10 @@ const Home = () => {
           </ul>
         </div>
       </div>
-  
-      <Heattmap />
+
+      <Heatmap />
     </div>
   );
 };
 
 export default Home;
-
