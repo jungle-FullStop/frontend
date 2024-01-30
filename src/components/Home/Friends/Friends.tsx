2b893fe931169171244488d92880bf30ee44d4c2
Changjo Hwang <hchj2005@gmail.com>
2024-01-31 00:53:05 +0900

    feat: TIL 랭킹 위치 수정

--- src/components/Home/Friends/Friends.tsx
+++ src/components/Home/Friends/Friends.tsx
@@ -27,22 +27,27 @@ export const Friends = () => {
       <Typography className="text-center text-2xl font-bold">TIL 연속 작성 순위</Typography>
       <div className="grid grid-cols-3">
         {friendListData.data.friends.length !== 0 ? (
-          friendListData.data.friends.map((data: FriendListResponse, index: number) => (
-            <div className="flex flex-col items-center" key={index}>
-              <Typography className="font-extrabold">{index + 1}위</Typography>
-              <img
-                className={`border-brown-25 h-20 rounded-full border-2 border-solid object-cover transition duration-1000 ease-in-out sm:mb-0`}
-                src={data.profileImage ? data.profileImage : anonymousImage}
-                alt="프로필 사진"
-              />
-              <Typography className="font-bold">{data.name}</Typography>
-              <Typography className="font-bold text-green-500">현재 {data.tilScore}일</Typography>
-            </div>
-          ))
+          friendListData.data.friends
+            .map((data: FriendListResponse, index: number) => (
+              <div className="flex flex-col items-center" key={index}>
+                <Typography className="font-extrabold">{index + 1}위</Typography>
+                <img
+                  className={`border-brown-25 h-20 rounded-full border-2 border-solid object-cover transition duration-1000 ease-in-out sm:mb-0`}
+                  src={data.profileImage ? data.profileImage : anonymousImage}
+                  alt="프로필 사진"
+                />
+                <Typography className="font-bold">{data.name}</Typography>
+                <Typography className="font-bold text-green-500">현재 {data.tilScore}일</Typography>
+              </div>
+            ))
+            .slice(0, 3)
         ) : (
-          <div className="flex w-full flex-col items-center justify-center gap-3">
-            <p className="font-bold">아직 친구가 없어요.</p>
-          </div>
+          <>
+            <div></div>
+            <div className="flex w-full flex-col items-center justify-center gap-3">
+              <p className="font-bold">아직 친구가 없어요.</p>
+            </div>
+          </>
         )}
       </div>
     </div>
