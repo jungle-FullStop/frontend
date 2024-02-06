d8fbce8b7039129e7cb6cab34096cf32a0794043
박정민 <v5833165v@naver.com>
2024-02-06 10:50:37 +0900

    구글 URL 추가

--- src/util/Constants/constants.ts
+++ src/util/Constants/constants.ts
@@ -1,3 +1,5 @@
+import { isDevelopment } from '../../../vite.config.ts';
+
 export const PAGE_URL = {
   LANDING: '/',
   HOME: '/home',
@@ -12,9 +14,14 @@ export const PAGE_URL = {
 
 export const WAITING_TIME = 5;
 
+const redirect_page = isDevelopment ? 'http://localhost:5173/' : 'https://tilfullstop.site/';
+import.meta.env.DEV;
+
 export const GOOGLE_LOGIN_FORM_URL =
   'https://accounts.google.com/o/oauth2/v2/auth?response_type=code&' +
-  'redirect_uri=http://localhost:5173/auth&' +
+  'redirect_uri=' +
+  redirect_page +
+  'auth&' +
   'client_id=998666507743-8fe6chebijnoabeeql68cat66bbit1vj.apps.googleusercontent.com&' +
   'scope=https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email&' +
   'access_type=offline';
