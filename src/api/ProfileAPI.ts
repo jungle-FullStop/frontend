584691951637e2dc83fa9a41af8665009a959b85
박정민 <v5833165v@naver.com>
2024-02-09 10:13:29 +0900

    수정후 배포

--- src/api/ProfileAPI.ts
+++ src/api/ProfileAPI.ts
@@ -12,3 +12,17 @@ export const getProfile = async (userId: number) => {
     return null;
   }
 };
+
+// 아래 코드 싹다 지우고 다시 구현해야함. 배포할 때 오류 생겨서 임시로 박아놓음
+export const updateProfile = async (userId: number, formData: any) => {
+  // 구현 내용
+  try {
+    const response = await interceptor(API_PATH.USER.userProfile(userId), {
+      credentials: 'include',
+    });
+    return await response.json();
+  } catch (error) {
+    console.error('프로필 불러오기에 실패했습니다.', error);
+    return null;
+  }
+};
