e15db7475f1a941ca7a82ea2f262d8b4e2b888ca
Changjo Hwang <hchj2005@gmail.com>
2024-02-09 20:48:13 +0900

    feat: 팀로고 추가

--- src/components/Common/Footer.tsx
+++ src/components/Common/Footer.tsx
@@ -1,14 +1,11 @@
 import { Typography } from '@material-tailwind/react';
+import teamLogoBlack from '@assets/image/teamLogoBlack.png';
 
 export function Footer() {
   return (
     <footer className="w-full bg-white p-8">
       <div className="flex flex-row flex-wrap items-center justify-center gap-x-12 gap-y-6 bg-white text-center md:justify-between">
-        <img
-          src="https://docs.material-tailwind.com/img/logo-ct-dark.png"
-          alt="logo-ct"
-          className="w-10"
-        />
+        <img src={teamLogoBlack} alt="logo-ct" className="w-[200px]" />
         <ul className="flex flex-wrap items-center gap-x-8 gap-y-2">
           <li>
             <Typography
