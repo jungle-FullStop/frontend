51e7e88dc7f9d08e9e6c7176cbb5c12272d8e95d
Changjo Hwang <hchj2005@gmail.com>
2024-02-12 19:39:35 +0900

    feat: TIL 작성하기 hook 수정

--- src/pages/Board.tsx
+++ src/pages/Board.tsx
@@ -2,6 +2,7 @@ import NavBar from '@/components/Common/NavBar';
 import { CardDefault } from '@components/Board/CardDefault';
 import { useNavigate } from 'react-router-dom';
 import { useFindUserBoard } from '@hooks/useFindUserBoard.ts';
+import { WriteTIL } from '@components/Home/WriteTIL.tsx';
 
 export const Board = () => {
   const navigate = useNavigate();
@@ -9,30 +10,35 @@ export const Board = () => {
 
   return (
     <div className="main-container">
-      <NavBar></NavBar>
+      <NavBar />
       <div className="w-[90%}">
         <div className="TIL-container">
-          {cardList
-            ? cardList.map((data: any, items: number) => {
-                return (
-                  <div
-                    className="TIL opacity-85 shadow-xl"
-                    key={items}
-                    onClick={() => {
-                      navigate(`/board/${data.id}`);
-                    }}
-                  >
-                    <CardDefault
-                      cardTitle={data.title}
-                      cardContents={data.contents}
-                      cardDate={data.timestamp}
-                      userImage={userImage}
-                      userName={userName}
-                    />
-                  </div>
-                );
-              })
-            : null}
+          {cardList.length > 0 ? (
+            cardList.map((data: any, items: number) => {
+              return (
+                <div
+                  className="TIL opacity-85 shadow-xl"
+                  key={items}
+                  onClick={() => {
+                    navigate(`/board/${data.id}`);
+                  }}
+                >
+                  <CardDefault
+                    cardTitle={data.title}
+                    cardContents={data.contents}
+                    cardDate={data.timestamp}
+                    userImage={userImage}
+                    userName={userName}
+                  />
+                </div>
+              );
+            })
+          ) : (
+            <div className="mx-auto flex h-[500px] flex-col justify-center gap-y-8">
+              <p className="text-5xl font-bold">아직 작성하신 TIL이 없어요.</p>
+              <WriteTIL color={'yellow'} />
+            </div>
+          )}
         </div>
       </div>
     </div>
