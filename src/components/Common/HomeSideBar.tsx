01d5110985a8aef679cbd43d70c627bb3e31ec67
Changjo Hwang <hchj2005@gmail.com>
2024-02-14 15:36:37 +0900

    fix: 모드 변경시 프로필 수정

--- src/components/Common/HomeSideBar.tsx
+++ src/components/Common/HomeSideBar.tsx
@@ -3,6 +3,7 @@ import { WriteTIL } from '@components/Home/WriteTIL.tsx';
 import { Quotes } from '@components/Home/Quotes.tsx';
 import { Ad } from '@components/Common/Ad.tsx';
 import useThrottleScroll from '@hooks/Common/useThrottleScroll.ts';
+import { TeamProfile } from '@components/Home/Profile/TeamProfile.tsx';
 
 export const HomeSideBar = (props: any) => {
   const barPosition = useThrottleScroll(100, 50, 450);
@@ -10,7 +11,7 @@ export const HomeSideBar = (props: any) => {
     <div className={'sidebar-container'}>
       <div className={'sidebar'} style={{ transform: `translateY(${barPosition}px)` }}>
         <div className="flex flex-col gap-y-3">
-          <UserProfile />
+          {props.mode === 'user' ? <UserProfile /> : <TeamProfile />}
           <WriteTIL color={props.color} flipCard={props.flipCard} />
           <Quotes mode={props.mode} />
           <Ad mode={'jungle'} />
