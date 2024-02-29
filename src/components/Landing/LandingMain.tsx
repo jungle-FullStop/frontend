fdaa78e13024c188fbc3eccb34fa1ffcc4871ced
Changjo Hwang <hchj2005@gmail.com>
2024-03-01 00:02:57 +0900

    feat: 랜딩 메인페이지 추가

--- src/components/Landing/LandingMain.tsx
+++ src/components/Landing/LandingMain.tsx
@@ -1,8 +1,9 @@
 export const LandingMain = () => {
   return (
     <div>
-      <p className="text-4xl font-bold text-orange-500">TIL을 쓰지 않으면,</p>
-      <p className="mb-3 text-4xl font-bold text-orange-500">나의 하루는 끝나지 않는다</p>
+      <p className="mb-3 text-4xl font-bold text-orange-500">
+        TIL을 쓰지 않으면, <div /> 나의 하루는 끝나지 않는다
+      </p>
       <p className="mb-7 text-8xl font-black text-amber-500">티.나.끝</p>
       <div id="LoreTextContainer" className="mb-14">
         <p className="text-4xl font-bold text-black">티.나.끝의 가이드라인과 함께</p>
--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,37 +1,17 @@
 import { Header } from '@components/Landing/Header';
-import { LandingMain } from '@/components/Landing/LandingMain';
-import { LoginButton } from '@/components/Landing/LoginButton';
-import HeroImage from '@/assets/image/heroImage.png';
+import LandingMainSection from '@components/Landing/LandingMainSection.tsx';
+import { Footer } from '@components/Common/Footer.tsx';
+import { Setting } from '@components/Common/Setting.tsx';
 
 const Landing = () => {
   return (
-    <div className="flex h-screen w-screen flex-col">
+    <>
       <Header />
-      <div className="mx-auto flex w-[75%] flex-grow items-center justify-between">
-        <div id="Main" className="flex flex-col">
-          <LandingMain />
-          <LoginButton />
-        </div>
-        <div className={'w-[45%]'}>
-          <img src={HeroImage} alt="landing" />
-        </div>
-      </div>
-      <div className={'mx-auto mb-10 flex flex-row items-center'}>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 64 64"
-          className={'landing-arrow mr-3 w-10'}
-        >
-          <path
-            d="M28.109 8.001h7.132v34.626l11.063-11.069 5.038 5.04-19.667 19.674L12 36.601l5.046-5.043 11.063 11.069Zm5.654 1.478h-4.176v36.717L17.045 33.652l-2.954 2.95 17.584 17.58 17.577-17.581-2.948-2.949-12.541 12.544Z"
-            fill={'#ff9800'}
-          />
-        </svg>
-        <p className="w-[175px] text-center text-2xl font-bold text-orange-500">
-          티.나.끝 서비스는?
-        </p>
-      </div>
-    </div>
+      <LandingMainSection />
+      <div className={'landing-container'}></div>
+      <Setting />
+      <Footer />
+    </>
   );
 };
 
