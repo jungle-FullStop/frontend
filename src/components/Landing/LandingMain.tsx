904aa05a74f07cc9bd86d2615757fbeb54a4ff87
Changjo Hwang <hchj2005@gmail.com>
2024-02-29 23:32:54 +0900

    feat: 랜딩페이지 개선

--- src/components/Landing/LandingMain.tsx
+++ src/components/Landing/LandingMain.tsx
@@ -1,12 +1,12 @@
 export const LandingMain = () => {
   return (
     <div>
-      <p className="text-5xl font-semibold text-orange-500">TIL을 쓰지 않으면,</p>
-      <p className="mb-3 text-5xl font-semibold text-orange-500">나의 하루는 끝나지 않는다</p>
-      <p className="mb-7 text-7xl font-black text-amber-500">티.나.끝</p>
+      <p className="text-4xl font-bold text-orange-500">TIL을 쓰지 않으면,</p>
+      <p className="mb-3 text-4xl font-bold text-orange-500">나의 하루는 끝나지 않는다</p>
+      <p className="mb-7 text-8xl font-black text-amber-500">티.나.끝</p>
       <div id="LoreTextContainer" className="mb-14">
-        <p className="text-3xl font-extrabold text-white">티.나.끝의 가이드라인과 함께</p>
-        <p className="text-3xl font-extrabold text-white">TIL을 작성해보세요.</p>
+        <p className="text-4xl font-bold text-black">티.나.끝의 가이드라인과 함께</p>
+        <p className="text-4xl font-bold text-black">TIL을 작성해보세요.</p>
       </div>
     </div>
   );
